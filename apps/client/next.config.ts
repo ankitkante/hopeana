370a821e871a0f105afa585d303c78b737cff921
Ankit Kante <ankit.kante@falkonry.com>
2026-02-26 16:11:03 +0530

    Switch to netlify proxy for posthog

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  skipTrailingSlashRedirect: true,
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://us.i.posthog.com/decide",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
